6b79dde30aebc3aa747cc35a2650954fdc45dcf5
ahmedsilinii <ahmedsilini5@gmail.com>
2024-04-01 00:25:46 +0200

    Quiz Widget added

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Start Screen'),
-    );
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+          color: const Color.fromARGB(150, 255, 255, 255),
+        ),
+        // Opacity(
+        //   opacity: 0.5,
+        //   child: Image.asset(
+        //     'assets/images/quiz-logo.png',
+        //     width: 300,
+        //   ),
+        // ),
+        const SizedBox(height: 80),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+        const SizedBox(height: 30),
+        ElevatedButton.icon(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.white),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            ),
+          ),
+          icon: const Icon(Icons.play_arrow, color: Colors.blue),
+          label: const Text('Start Quiz'),
+        ),
+      ],
+    ));
   }
 }
