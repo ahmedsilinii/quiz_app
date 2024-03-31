1e7b9b57c1fb6fec2f9b2ec36d8571612a07f50e
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-31 23:54:28 +0200

    updated the background

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF64B5F6),
+                Color(0xFF1976D2),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
